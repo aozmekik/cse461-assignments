2f8c14368645373584e9d4ea3e4d056f55dff29a
Ahmed Semih Özmekik <ahmed-semih@outlook.com>
2021-05-11 17:53:56 +0300

    basic 3d home done

--- hw1/test.cpp
+++ hw1/test.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 
 float angle = -45;
-float xScale = 1, yScale = 1, zAngle = 0;
+float xScale = 1, yScale = 1;
+float rotate = 2;
 
 void rotateFunc(int key, int x, int y)
 {
@@ -36,48 +37,167 @@ void rotateFunc(int key, int x, int y)
     glutPostRedisplay();
 }
 
-void initGL()
-{
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClearDepth(1.0f);
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LEQUAL);
-    glShadeModel(GL_SMOOTH);
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
-}
-
 // sekillere rotate ve scale transformu uygulamayı saglar.
 // her cizimden once tekrar cagrilmali.
 void registerTransform()
 {
+    glTranslatef(0, 0, -6);
     glRotatef(angle, 0, 1, 0);
     glScalef(xScale, yScale, 1);
 }
 
 void surface()
 {
-    glLoadIdentity();
-
     // simulasyon yuzeyi, acik yesil.
-    glTranslatef(0, 0, -6);
+    glLoadIdentity();
     glColor3f(0.56, .93, 0.56);
     registerTransform();
     glBegin(GL_QUADS);
-    glVertex3f(-40, -1, 50);
-    glVertex3f(-40, -1, -50);
-    glVertex3f(40, -1, -50);
-    glVertex3f(40, -1, 50);
+    glVertex3f(-50, -1, 50);
+    glVertex3f(-50, -1, -50);
+    glVertex3f(50, -1, -50);
+    glVertex3f(50, -1, 50);
     glEnd();
 }
 
-void homeBody()
+void windows()
+{
+    glColor3f(0.2, 0.5, 0.9);
+    glPushMatrix();
+    registerTransform();
+    glBegin(GL_QUADS);
+        glColor3f(0.6, 0.4, 0.2);
+
+    glVertex3f(2.01, 0.2, -0.3);
+    glVertex3f(2.01, 0.2, 0.4);
+    glVertex3f(2.01, -1, 0.4);
+    glVertex3f(2.01, -1, -0.3);
+    glEnd();
+    glPopMatrix();
+
+    glColor3f(0.2, 0.5, 0.9);
+    glPushMatrix();
+    registerTransform();
+    glBegin(GL_QUADS);
+    glVertex3f(2.01, 0.2, -1);
+    glVertex3f(2.01, 0.2, -1.7);
+    glVertex3f(2.01, -0.3, -1.7);
+    glVertex3f(2.01, -0.3, -1);
+    glEnd();
+    glPopMatrix();
+
+    glPushMatrix();
+    registerTransform();
+    glBegin(GL_LINES);
+    glColor3f(0, 0, 0);
+    glVertex3f(2.01, -0.1, -1);
+    glVertex3f(2.01, -0.1, -1.7);
+    glVertex3f(2.01, -0.2, -1.7);
+    glVertex3f(2.01, -0.2, -1);
+    glEnd();
+    glPopMatrix();
+
+    glColor3f(0.2, 0.5, 0.9);
+    glPushMatrix();
+    registerTransform();
+    glBegin(GL_QUADS);
+    glVertex3f(2.01, 0.2, 1.8);
+    glVertex3f(2.01, 0.2, 1.1);
+    glVertex3f(2.01, -0.3, 1.1);
+    glVertex3f(2.01, -0.3, 1.8);
+    glEnd();
+    glPopMatrix();
+
+    glPushMatrix();
+    registerTransform();
+    glBegin(GL_LINES);
+    glColor3f(0, 0, 0);
+    glVertex3f(2.01, -0.1, 1.8);
+    glVertex3f(2.01, -0.1, 1.1);
+    glVertex3f(2.01, -0.2, 1.1);
+    glVertex3f(2.01, -0.2, 1.8);
+    glEnd();
+    glPopMatrix();
+}
+
+void chimney()
+{
+    // baca, dikdörtgenler prizması
+    glPushMatrix();
+    registerTransform();
+    glBegin(GL_QUADS);
+    glColor3f(0.5, 0.5, 0.8);
+
+    glVertex3f(1.5, 1.0, 0);
+    glVertex3f(1.5, 1.8, 0);
+    glVertex3f(1.5, 1.8, -0.5);
+    glVertex3f(1.5, 1.0, -0.5);
+
+    glVertex3f(1, 1.5, -0.5);
+    glVertex3f(1, 1.8, -0.5);
+    glVertex3f(1, 1.8, 0);
+    glVertex3f(1, 1.5, 0);
+
+    glVertex3f(1, 1.0, 0);
+    glVertex3f(1.5, 1.0, 0);
+    glVertex3f(1.5, 1.8, 0);
+    glVertex3f(1, 1.8, 0);
+
+    glVertex3f(1, 1.8, -0.5);
+    glVertex3f(1.5, 1.8, -0.5);
+    glVertex3f(1.5, 1.0, -0.5);
+    glVertex3f(1, 1.0, -0.5);
+    glEnd();
+    glPopMatrix();
+}
+
+void roof()
+{
+    // evin catisi, icin ucgen prizma
+    glColor3f(0.6f, 0.02f, 0.02f); // kahverengi
+    glPushMatrix();
+    registerTransform();
+    glBegin(GL_QUADS);
+    glVertex3f(2.0, 1.0, -2.0);
+    glVertex3f(1, 1.5, -2.0);
+    glColor3f(0.65f, 0.1f, 0.1f);
+
+    glVertex3f(1, 1.5, 2.0);
+    glVertex3f(2.0, 1.0, 2.0);
+    glEnd();
+    glPopMatrix();
+
+    glPushMatrix();
+    registerTransform();
+    glBegin(GL_QUADS);
+    glVertex3f(0, 1.0, -2.0);
+    glVertex3f(1, 1.5, -2.0);
+    glColor3f(0.65f, 0.1f, 0.1f);
+    glVertex3f(1, 1.5, 2.0);
+    glVertex3f(0, 1.0, 2.0);
+    glEnd();
+    glPopMatrix();
+
+    glPushMatrix();
+    registerTransform();
+    glBegin(GL_TRIANGLES);
+    glVertex3f(0, 1.0, -2.0);
+    glVertex3f(2, 1.0, -2.0);
+    glVertex3f(1, 1.5, -2.0);
+
+    glEnd();
+    glPopMatrix();
+}
+
+void front()
 {
     glLoadIdentity();
 
     // evin iskeleti, dikdortgenler prizması
+    glPushMatrix();
     glColor3f(0.2f, 0.1f, 0.03); // kahverengi
-    glTranslatef(0, 0.0f, -6.0f);
     registerTransform();
+    glTranslatef(1, 0, 0);
     glBegin(GL_QUADS);
 
     // prizma icin 6 farkli yuzey
@@ -111,16 +231,15 @@ void homeBody()
     glVertex3f(1.0f, -1.0f, 2.0f);
     glVertex3f(1.0f, -1.0f, -2.0f);
     glEnd();
+    glPopMatrix();
+}
 
-    glTranslatef(0, 0, -6);
-    glColor3f(0.9f, 0.2f, 0.5); // kahverengi
-    registerTransform();
-    glBegin(GL_TRIANGLES);
-    glVertex3f(2, 0, 1.1);
-    glVertex3f(1.9, .8, 0);
-
-    glVertex3f(2, 0, -1.1);
-    glEnd();
+void homeBody()
+{
+    front();
+    roof();
+    chimney();
+    windows();
 }
 
 void display()
@@ -128,26 +247,27 @@ void display()
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
 
-    // kamerayi evi ortalayacak konuma getir
-    glTranslatef(-4.0f, 0, 4.0f);
-
     surface();
     homeBody();
+    // // kamerayi evi ortalayacak konuma getir
+    // glTranslatef(0.0f, 0, 10.0f);
 
     glutSwapBuffers();
 }
 
-void reshape(GLsizei width, GLsizei height)
+static void reshape(GLsizei width, GLsizei height)
 {
     if (height == 0)
         height = 1;
-    GLfloat aspect = (GLfloat)width / (GLfloat)height;
+    const GLfloat aspect = (GLfloat)width / (GLfloat)height;
 
     glViewport(0, 0, width, height);
-
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+
+    glFrustum(-aspect, aspect, -1.0, 1.0, 2.0, 100.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 }
 
 int main(int argc, char **argv)
@@ -157,6 +277,8 @@ int main(int argc, char **argv)
     glutInitWindowPosition(2000, 100);
     glutInitWindowSize(1600, 900);
     glutCreateWindow("3B EV");
+    glEnable(GL_DEPTH_TEST);
+
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
     glutSpecialFunc(rotateFunc);
